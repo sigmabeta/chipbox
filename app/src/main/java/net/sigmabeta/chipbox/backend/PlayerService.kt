f789a1ce26e20a7f97e9cb1f50c1c5122836b9ac
sigmabeta <bastos.eder@gmail.com>
2016-01-05 23:56:14 -0500

    Hook up notifications to the reactive subject

--- app/src/main/java/net/sigmabeta/chipbox/backend/PlayerService.kt
+++ app/src/main/java/net/sigmabeta/chipbox/backend/PlayerService.kt
@@ -58,6 +58,10 @@ class PlayerService : Service(), BackendView {
 
         // A workaround for the fact that controllerCallback is null inside the init {} constructor.
         notificationManager?.setControllerCallback()
+
+        notificationManager?.subscribeToUpdates()
+
+        notificationManager?.startNotification()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -82,6 +86,8 @@ class PlayerService : Service(), BackendView {
         session?.release()
 
         player?.backendView = null
+
+        notificationManager?.unsubscribeFromUpdates()
     }
 
     override fun onBind(intent: Intent): IBinder? {
@@ -97,16 +103,12 @@ class PlayerService : Service(), BackendView {
 
         session?.isActive = true
         registerNoisyReceiver()
-
-        updatePlaybackState(null)
     }
 
     override fun pause() {
         logVerbose("[PlayerService] Processed PAUSE command.")
 
         unregisterNoisyReceiver()
-
-        updatePlaybackState(null)
     }
 
     override fun stop() {
@@ -115,18 +117,15 @@ class PlayerService : Service(), BackendView {
         session?.isActive = false
         unregisterNoisyReceiver()
 
-        updatePlaybackState(null)
         stopSelf()
     }
 
     override fun skipToNext() {
         logVerbose("[PlayerService] Processed NEXT command.")
-        updatePlaybackState(null)
     }
 
     override fun skipToPrev() {
         logVerbose("[PlayerService] Processed PREV command.")
-        updatePlaybackState(null)
     }
 
     /**
@@ -145,92 +144,8 @@ class PlayerService : Service(), BackendView {
         ServiceInjector.inject(this)
     }
 
-    /**
-     * Update the current media player state, optionally showing an error message.
-
-     * @param error if not null, error message to present to the user.
-     */
-    private fun updatePlaybackState(error: String?) {
-        logDebug("[PlayerService] Updating playback state: ${player?.state}")
-
-        var position = player?.position ?: PlaybackState.PLAYBACK_POSITION_UNKNOWN
-
-        var state = player?.state
-
-        val track = player?.playingTrack
-
-        val metadata = if (track != null) {
-            val imagesFolderPath = getExternalFilesDir(null).absolutePath + "/images/"
-            val imagePath = imagesFolderPath + track.gameId.toString() + "/local.png"
-
-            val metadataBuilder = Track.toMetadataBuilder(track)
-
-            // TODO May need to do this asynchronously.
-            val imageBitmap = BitmapFactory.decodeFile(imagePath)
-
-            if (imageBitmap != null) {
-                metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, imageBitmap)
-            } else {
-                logError("[PlayerService] Couldn't load game art.")
-            }
-
-            metadataBuilder.build()
-        } else {
-            null
-        }
-
-        session?.setMetadata(metadata)
-
-        val stateBuilder = PlaybackStateCompat.Builder().setActions(getAvailableActions())
-
-        // If there is an error message, send it to the playback state:
-        if (error != null) {
-            // Error states are really only supposed to be used for errors that cause playback to
-            // stop unexpectedly and persist until the user takes action to fix it.
-            stateBuilder.setErrorMessage(error)
-            state = PlaybackState.STATE_ERROR
-        }
-
-        logVerbose("[PlayerService] Playback state: ${state} \n" +
-                "Track: ${track}\n" +
-                "Metadata: ${metadata?.description}\n")
-
-        stateBuilder.setState(state ?: PlaybackState.STATE_ERROR, position, 1.0f, SystemClock.elapsedRealtime())
-
-        session?.setPlaybackState(stateBuilder.build())
-
-        if (state == PlaybackState.STATE_PLAYING || state == PlaybackState.STATE_PAUSED
-                && !(notificationManager?.notified ?: false)) {
-            notificationManager?.startNotification()
-        }
-    }
-
     private fun getPlayerActivityIntent(): PendingIntent {
         return PendingIntent.getActivity(this, 0, PlayerActivity.getLauncher(this), 0)
-    }
-
-    private fun getAvailableActions(): Long {
-        var actions = PlaybackState.ACTION_PLAY or PlaybackState.ACTION_STOP
-
-        if (player?.state == PlaybackState.STATE_PLAYING) {
-            actions = actions or PlaybackState.ACTION_PAUSE
-        }
-
-        val playbackQueuePosition = player?.playbackQueuePosition
-        val playbackQueueSize = player?.playbackQueue?.size
-
-        if (playbackQueuePosition != null) {
-            if (playbackQueuePosition > 0) {
-                actions = actions or PlaybackState.ACTION_SKIP_TO_PREVIOUS
-            }
-            if (playbackQueueSize != null) {
-                if (playbackQueuePosition < playbackQueueSize - 1) {
-                    actions = actions or PlaybackState.ACTION_SKIP_TO_NEXT
-                }
-            }
-        }
-
-        return actions
     }
 
     private fun registerNoisyReceiver() {
