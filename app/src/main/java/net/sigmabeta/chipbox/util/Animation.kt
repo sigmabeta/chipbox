0e0e3261d3f5b0665977e6f130d560df4762295d
sigmabeta <bastos.eder@gmail.com>
2016-02-14 01:58:09 -0500

    Add animation for coordinator layout when now playing goes away

--- app/src/main/java/net/sigmabeta/chipbox/util/Animation.kt
+++ app/src/main/java/net/sigmabeta/chipbox/util/Animation.kt
@@ -2,27 +2,36 @@ package net.sigmabeta.chipbox.util
 
 import android.view.View
 import android.view.ViewPropertyAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 
 val accelerateInterpolator = AccelerateInterpolator()
 val decelerateInterpolator = DecelerateInterpolator()
+val accDecelerateInterpolator = AccelerateDecelerateInterpolator()
 
-fun View.slideViewDown(): ViewPropertyAnimator {
+fun View.slideViewOffscreen(): ViewPropertyAnimator {
     return animate()
             .withLayer()
             .setInterpolator(accelerateInterpolator)
-            .setDuration(300)
+            .setDuration(400)
             .translationY(height.toFloat())
 }
 
-fun View.slideViewUp(): ViewPropertyAnimator {
+fun View.slideViewOnscreen(): ViewPropertyAnimator {
     visibility = View.VISIBLE
 
     return animate()
             .withLayer()
             .setInterpolator(decelerateInterpolator)
-            .setDuration(300)
+            .setDuration(400)
             .translationY(0.0f)
 }
 
+fun View.slideViewToProperLocation(): ViewPropertyAnimator {
+    return animate()
+            .withLayer()
+            .setInterpolator(accDecelerateInterpolator)
+            .setDuration(400)
+            .translationY(0.0f)
+}
