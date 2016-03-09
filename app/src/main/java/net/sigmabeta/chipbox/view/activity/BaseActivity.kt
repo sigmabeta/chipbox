af436a97ed307676d71240a43f1ced55e9b0efc3
sigmabeta <bastos.eder@gmail.com>
2016-03-08 21:41:47 -0500

    Add more detalied base classes

--- app/src/main/java/net/sigmabeta/chipbox/view/activity/BaseActivity.kt
+++ app/src/main/java/net/sigmabeta/chipbox/view/activity/BaseActivity.kt
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import net.sigmabeta.chipbox.ChipboxApplication
+import net.sigmabeta.chipbox.presenter.ActivityPresenter
 import net.sigmabeta.chipbox.util.logError
+import net.sigmabeta.chipbox.view.interfaces.BaseView
 
-public abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), BaseView {
     /**
      * Calls the superclass constructor, and then automatically
      * requests an injection of the Activity's dependencies.
@@ -19,14 +21,31 @@ public abstract class BaseActivity : AppCompatActivity() {
         if (ChipboxApplication.appComponent == null) {
             logError("[DaggerActivity] AppComponent null.")
         }
+
         inject()
+        getPresenter().onCreate(intent.extras, savedInstanceState, this)
     }
 
-    fun showToastMessage(message: String) {
+    override fun onResume() {
+        super.onResume()
+        getPresenter().onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        getPresenter().onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        getPresenter().onDestroy(isFinishing)
+    }
+
+    override fun showToastMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    fun showErrorSnackbar(message: String, action: View.OnClickListener?, actionLabel: Int?) {
+    override fun showErrorSnackbar(message: String, action: View.OnClickListener?, actionLabel: Int?) {
         val snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
 
         if (action != null && actionLabel != null) {
@@ -41,4 +60,12 @@ public abstract class BaseActivity : AppCompatActivity() {
      * and do any other necessary setup.
      */
     protected abstract fun inject()
+
+    protected abstract fun getPresenter(): ActivityPresenter
+
+    /**
+     * Perform any necessary run-time setup of views: RecyclerViews, ClickListeners,
+     * etc.
+     */
+    protected abstract fun configureViews()
 }
