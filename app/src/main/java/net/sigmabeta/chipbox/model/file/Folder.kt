474e71008a924aab7bcd71f44e236298dfb90dc8
sigmabeta <bastos.eder@gmail.com>
2016-03-18 11:12:17 -0400

    Implement all Folder related tasks in DBFlow

---
+++ app/src/main/java/net/sigmabeta/chipbox/model/file/Folder.kt
@@ -0,0 +1,19 @@
+package net.sigmabeta.chipbox.model.file
+
+import com.raizlabs.android.dbflow.annotation.PrimaryKey
+import com.raizlabs.android.dbflow.annotation.Table
+import com.raizlabs.android.dbflow.annotation.Unique
+import com.raizlabs.android.dbflow.structure.BaseModel
+import net.sigmabeta.chipbox.ChipboxDatabase
+
+@Table(database = ChipboxDatabase::class, allFields = true)
+class Folder() : BaseModel() {
+    constructor(path: String) : this() {
+        this.path = path
+    }
+
+    @PrimaryKey var id: Long? = null
+
+    @Unique
+    var path: String? = null
+}
