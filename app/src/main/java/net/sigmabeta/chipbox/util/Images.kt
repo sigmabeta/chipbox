2b03b46e40b87fb3a71bd6ed2197cfc273b4d6ac
sigmabeta <bastos.eder@gmail.com>
2016-03-24 08:51:41 -0400

    Have Picasso render Bitmaps for MediaSession Notifications

--- app/src/main/java/net/sigmabeta/chipbox/util/Images.kt
+++ app/src/main/java/net/sigmabeta/chipbox/util/Images.kt
@@ -1,10 +1,26 @@
 package net.sigmabeta.chipbox.util
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.widget.ImageView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import net.sigmabeta.chipbox.R
+import rx.Observable
+
+
+fun loadBitmapLowQuality(context: Context, path: String): Observable<Bitmap> {
+    return Observable.create {
+        val bitmap = Picasso.with(context)
+                .load(path)
+                .config(Bitmap.Config.RGB_565)
+                .error(R.drawable.img_album_art_blank)
+                .get()
+
+        it.onNext(bitmap)
+        it.onCompleted()
+    }
+}
 
 fun ImageView.loadImageLowQuality(path: String, fade: Boolean, placeholder: Boolean, callback: Callback? = null) {
     val requestCreator = Picasso.with(context)
