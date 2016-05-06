484ecae6b329007f6efebdf8dcb7e873a41a313d
sigmabeta <bastos.eder@gmail.com>
2016-05-05 20:33:12 -0400

    Upgrade to gradle plugin 2.1

--- app/src/main/java/net/sigmabeta/chipbox/ui/platform/PlatformViewHolder.kt
+++ app/src/main/java/net/sigmabeta/chipbox/ui/platform/PlatformViewHolder.kt
@@ -4,7 +4,6 @@ import android.view.View
 import kotlinx.android.synthetic.main.list_item_platform.view.*
 import net.sigmabeta.chipbox.model.domain.Platform
 import net.sigmabeta.chipbox.ui.BaseViewHolder
-import net.sigmabeta.chipbox.ui.platform.PlatformListAdapter
 
 class PlatformViewHolder(view: View, adapter: PlatformListAdapter) : BaseViewHolder<Platform, PlatformViewHolder, PlatformListAdapter>(view, adapter), View.OnClickListener {
     var platformId: Long? = null
