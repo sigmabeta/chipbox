37c5dd7d227ed9efb1e7df20d13b6ccf16d4e961
sigmabeta <bastos.eder@gmail.com>
2017-02-22 15:21:01 -0500

    Refactor gme to use scanner & backend object

---
+++ app/src/main/java/net/sigmabeta/chipbox/backend/vgm/BackendImpl.kt
@@ -0,0 +1,27 @@
+package net.sigmabeta.chipbox.backend.vgm
+
+import net.sigmabeta.chipbox.backend.Backend
+
+class BackendImpl : Backend {
+    external override fun loadFile(filename: String, track: Int, sampleRate: Int, bufferSize: Long, fadeTimeMs: Long)
+
+    external override fun readNextSamples(targetBuffer: ShortArray)
+
+    external override fun getMillisPlayed(): Long
+
+    external override fun seek(timeInSec: Int): String
+
+    external override fun setTempo(tempo: Double)
+
+    external override fun getVoiceCount(): Int
+
+    external override fun getVoiceName(position: Int): String
+
+    external override fun muteVoice(voiceNumber: Int, enabled: Int)
+
+    external override fun isTrackOver(): Boolean
+
+    external override fun teardown()
+
+    external override fun getLastError(): String?
+}
