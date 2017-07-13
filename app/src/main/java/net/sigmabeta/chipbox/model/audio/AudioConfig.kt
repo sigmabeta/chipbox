a93b1dba33dc1306f5939b44168c31465d99c402
sigmabeta <bastos.eder@gmail.com>
2017-07-13 19:55:30 -0400

    Add debug settings screen

--- app/src/main/java/net/sigmabeta/chipbox/model/audio/AudioConfig.kt
+++ app/src/main/java/net/sigmabeta/chipbox/model/audio/AudioConfig.kt
@@ -1,7 +1,25 @@
 package net.sigmabeta.chipbox.model.audio
 
-data class AudioConfig(val sampleRate: Int,
-                       val bufferSizeBytes: Int,
-                       val minimumLatency: Int,
-                       val bufferSizeShorts: Int = bufferSizeBytes / 2,
-                       val bufferSizeSamples: Int = bufferSizeBytes / 4)
+ class AudioConfig(val sampleRate: Int,
+                       val minBufferSize: Int,
+                       var bufferSizeMultiplier: Int,
+                       var bufferCount: Int) {
+
+    val bufferSizeBytes
+        get() = minBufferSize * bufferCount
+
+    val bufferSizeShorts
+        get() = bufferSizeBytes / 2
+
+    val bufferSizeSamples
+        get() = bufferSizeBytes / 4
+
+    val minimumLatency
+        get() = 1000 * bufferSizeSamples / sampleRate
+
+    val actualLatency
+        get() =  minimumLatency * bufferSizeMultiplier
+
+    val totalBufferSizeMs
+        get() = bufferCount * actualLatency
+}
--- app/src/main/java/net/sigmabeta/chipbox/ui/main/MainPresenter.kt
+++ app/src/main/java/net/sigmabeta/chipbox/ui/main/MainPresenter.kt
@@ -26,6 +26,7 @@ class MainPresenter @Inject constructor(val player: Player,
         when (itemId) {
             R.id.drawer_refresh -> view?.startScanner()
             R.id.drawer_settings -> view?.launchSettingsActivity()
+            R.id.drawer_debug -> view?.launchDebugActivity()
             R.id.drawer_help -> view?.launchOnboarding()
         }
 
