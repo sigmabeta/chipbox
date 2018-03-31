34676bc456ceb099fe9973bd20801583de657dc2
sigmabeta <bastos.eder@gmail.com>
2018-03-31 08:22:38 -0400

    WIP Fix image loading slow

--- app/src/main/java/net/sigmabeta/chipbox/ui/game/GameActivity.kt
+++ app/src/main/java/net/sigmabeta/chipbox/ui/game/GameActivity.kt
@@ -7,8 +7,6 @@ import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Pair
 import android.view.View
-import android.view.View.VISIBLE
-import android.widget.FrameLayout
 import io.realm.OrderedCollectionChangeSet
 import kotlinx.android.synthetic.main.activity_game.*
 import net.sigmabeta.chipbox.BuildConfig
@@ -17,9 +15,8 @@ import net.sigmabeta.chipbox.model.domain.Game
 import net.sigmabeta.chipbox.model.domain.Track
 import net.sigmabeta.chipbox.ui.BaseActivity
 import net.sigmabeta.chipbox.ui.ListView
-import net.sigmabeta.chipbox.util.loadImageHighQuality
+import net.sigmabeta.chipbox.util.loadImageHighQualityThumbnailFirst
 import net.sigmabeta.chipbox.util.removeNullViewPairs
-import net.sigmabeta.chipbox.util.shrinktoNothing
 import javax.inject.Inject
 
 class GameActivity : BaseActivity<GamePresenter, GameView>(), GameView, ListView<Track, GameTrackViewHolder> {
@@ -46,17 +43,15 @@ class GameActivity : BaseActivity<GamePresenter, GameView>(), GameView, ListView
 
 
 
-    override fun setGame(game: Game?) {
+    override fun setGame(game: Game, width: Int, height: Int) {
         adapter.game = game
-        val imagePath = game?.artLocal
+        val imagePath = game.artLocal
 
         if (imagePath != null) {
-            image_hero_boxart.loadImageHighQuality(imagePath, false, false, getPicassoCallback())
+            image_hero_boxart.loadImageHighQualityThumbnailFirst(imagePath, width, height, getPicassoCallback())
         } else {
-            image_hero_boxart.loadImageHighQuality(Game.PICASSO_ASSET_ALBUM_ART_BLANK, false, false, getPicassoCallback())
+            image_hero_boxart.loadImageHighQualityThumbnailFirst(Game.PICASSO_ASSET_ALBUM_ART_BLANK,  width, height, getPicassoCallback())
         }
-
-        collapsing_toolbar.title = game?.title ?: "Error"
     }
 
     override fun setList(list: List<Track>) {
@@ -111,13 +106,13 @@ class GameActivity : BaseActivity<GamePresenter, GameView>(), GameView, ListView
         }
     }
 
+
+
     override fun getLayoutId(): Int {
         return R.layout.activity_game
     }
 
-    override fun getContentLayout(): FrameLayout {
-        return findViewById(android.R.id.content) as FrameLayout
-    }
+    override fun getContentLayout() = main_content
 
     override fun getSharedImage(): View? = image_hero_boxart
 
@@ -127,23 +122,25 @@ class GameActivity : BaseActivity<GamePresenter, GameView>(), GameView, ListView
      * Activity
      */
 
-    override fun onBackPressed() {
-        if (!alreadyFinishing) {
-            alreadyFinishing = true
-            if (button_fab.visibility == VISIBLE) {
-                button_fab.shrinktoNothing().withEndAction {
-                    supportFinishAfterTransition()
-                }
-            } else {
-                supportFinishAfterTransition()
-            }
-        }
-    }
+//    override fun onBackPressed() {
+//        if (!alreadyFinishing) {
+//            alreadyFinishing = true
+//            if (button_fab.visibility == VISIBLE) {
+//                button_fab.shrinktoNothing().withEndAction {
+//                    supportFinishAfterTransition()
+//                }
+//            } else {
+//                supportFinishAfterTransition()
+//            }
+//        }
+//    }
 
     companion object {
         val ACTIVITY_TAG = "${BuildConfig.APPLICATION_ID}.game"
 
         val ARGUMENT_GAME_ID = "${ACTIVITY_TAG}.game_id"
+        val ARGUMENT_GAME_IMAGE_WIDTH = "${ACTIVITY_TAG}.image.width"
+        val ARGUMENT_GAME_IMAGE_HEIGHT = "${ACTIVITY_TAG}.image.height"
 
         fun launch(context: Context, gameId: String) {
             val launcher = Intent(context, GameActivity::class.java)
@@ -157,11 +154,13 @@ class GameActivity : BaseActivity<GamePresenter, GameView>(), GameView, ListView
                    gameId: String,
                    navBar: Pair<View, String>?,
                    statusBar: Pair<View, String>?,
-                   imageView: Pair<View, String>?,
-                   background: Pair<View, String>?) {
+                   imageView: Pair<View, String>,
+                   background: Pair<View, String>) {
             val launcher = Intent(activity, GameActivity::class.java)
 
             launcher.putExtra(ARGUMENT_GAME_ID, gameId)
+            launcher.putExtra(ARGUMENT_GAME_IMAGE_WIDTH, imageView.first?.width)
+            launcher.putExtra(ARGUMENT_GAME_IMAGE_HEIGHT, imageView.first?.height)
 
             val sharedViewPairs = removeNullViewPairs(navBar, statusBar, imageView, background)
             val options = ActivityOptions.makeSceneTransitionAnimation(activity, *sharedViewPairs)
