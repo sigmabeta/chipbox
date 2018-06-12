238787b86ec8fbfec2c6e47b434e6bfa81f821db
sigmabeta <bastos.eder@gmail.com>
2018-06-12 12:41:15 -0400

    Enable viewholder kotlin extension caching

--- app/src/main/java/net/sigmabeta/chipbox/ui/BaseViewHolder.kt
+++ app/src/main/java/net/sigmabeta/chipbox/ui/BaseViewHolder.kt
@@ -2,13 +2,14 @@ package net.sigmabeta.chipbox.ui
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.extensions.LayoutContainer
 import net.sigmabeta.chipbox.model.domain.ListItem
 
 abstract class BaseViewHolder<T : ListItem, VH : BaseViewHolder<T, VH, A>, A : BaseArrayAdapter<T, VH>>(
-        val view: View,
-        val adapter: A) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        override val containerView: View,
+        val adapter: A) : RecyclerView.ViewHolder(containerView), View.OnClickListener, LayoutContainer {
     init {
-        view.setOnClickListener(this)
+        containerView.setOnClickListener(this)
     }
 
     override fun onClick(clicked: View) {
