11a108396ce6ff38b8ca0bf1bbd76ae70b88c38a
sigmabeta <bastos.eder@gmail.com>
2019-04-11 12:00:58 -0400

    Fix fadeout ending early

--- app/src/main/cpp/usf/android/Backend.cpp
+++ app/src/main/cpp/usf/android/Backend.cpp
@@ -258,7 +258,7 @@ void JNICALL Java_net_sigmabeta_chipbox_backend_usf_BackendImpl_readNextSamples
 //                            bytes_copied_from_sexyusf);
 
         processFadeOut(g_buffer_as_short_track,
-                       samples_to_read_track,
+                       g_buffer_length_samples_track,
                        g_played_sample_count_track,
                        g_fade_start_threshold_samples_track,
                        g_fade_length_samples_track);
