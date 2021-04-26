fb116359810271301db03fb736c89b1c504f472a
sigmabeta <bastos.eder@gmail.com>
2021-04-26 18:47:23 -0400

    Create enough modules to launch a blank RemasterActivity

--- core/activities/src/main/java/net/sigmabeta/chipbox/activities/RemasterActivity.kt
+++ core/activities/src/main/java/net/sigmabeta/chipbox/activities/RemasterActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import timber.log.Timber
 import javax.inject.Inject
 
 class RemasterActivity : AppCompatActivity(), HasAndroidInjector{
@@ -17,7 +18,7 @@ class RemasterActivity : AppCompatActivity(), HasAndroidInjector{
     override fun androidInjector() = androidInjector
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        setTheme(R.style.ChipboxImmersive)
+        setTheme(R.style.ChipboxImmersive)
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
 
@@ -27,10 +28,10 @@ class RemasterActivity : AppCompatActivity(), HasAndroidInjector{
         val widthPixels = displayMetrics.widthPixels
         val heightPixels = displayMetrics.heightPixels
 
-//        Timber.v("Device screen DPI: ${displayMetrics.densityDpi}")
-//        Timber.v("Device screen scaling factor: ${displayMetrics.density}")
-//        Timber.v("Device screen size: ${widthPixels}x$heightPixels")
-//        Timber.v("Device screen size (scaled): ${(widthPixels / displayMetrics.density).toInt()}" +
-//                "x${(heightPixels / displayMetrics.density).toInt()}")
+        Timber.v("Device screen DPI: ${displayMetrics.densityDpi}")
+        Timber.v("Device screen scaling factor: ${displayMetrics.density}")
+        Timber.v("Device screen size: ${widthPixels}x$heightPixels")
+        Timber.v("Device screen size (scaled): ${(widthPixels / displayMetrics.density).toInt()}" +
+                "x${(heightPixels / displayMetrics.density).toInt()}")
     }
 }
