3587d2631105b4003bc3c8345d08be96ebeebfd7
sigmabeta <bastos.eder@gmail.com>
2021-08-08 17:40:55 -0400

    Tweak animation of scan progress display

--- core/activities/src/main/java/net/sigmabeta/chipbox/activities/TopViewModel.kt
+++ core/activities/src/main/java/net/sigmabeta/chipbox/activities/TopViewModel.kt
@@ -1,14 +1,17 @@
 package net.sigmabeta.chipbox.activities
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.sample
 import net.sigmabeta.chipbox.scanner.Scanner
 import javax.inject.Inject
 
 class TopViewModel @Inject constructor(
     private val scanner: Scanner
 ):  ViewModel() {
+    @OptIn(FlowPreview::class)
+    val scannerEvents = scanner.scanEvents().sample(700L)
     val scannerStates = scanner.state()
-    val scannerEvents = scanner.scanEvents()
 
     fun startScan() {
         scanner.startScan()
