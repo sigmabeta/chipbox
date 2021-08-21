2ea808f166715806515a543683faa1abac1cb67a
sigmabeta <bastos.eder@gmail.com>
2021-08-21 14:18:28 -0400

    Setup DI structure for player control classes

--- features/game-detail/src/main/java/net/sigmabeta/chipbox/features/game_detail/GameDetailViewModel.kt
+++ features/game-detail/src/main/java/net/sigmabeta/chipbox/features/game_detail/GameDetailViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.sample
+import net.sigmabeta.chipbox.player.controls.Controls
 import net.sigmabeta.chipbox.repository.Repository
 import javax.inject.Inject
 
 @HiltViewModel
 class GameDetailViewModel @Inject constructor(
-    private val repository: Repository
+    private val repository: Repository,
+    private val controls: Controls
 ) : ViewModel() {
     var arguments: GameDetailArguments? = null
 
