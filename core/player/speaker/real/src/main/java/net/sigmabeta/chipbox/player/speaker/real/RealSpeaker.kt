e88d195f81ebdfe4f7844851e966fc4c6c7f7738
sigmabeta <bastos.eder@gmail.com>
2021-08-21 20:33:56 -0400

    Setup DI structure for player speaker classes

---
+++ core/player/speaker/real/src/main/java/net/sigmabeta/chipbox/player/speaker/real/RealSpeaker.kt
@@ -0,0 +1,9 @@
+package net.sigmabeta.chipbox.player.speaker.real
+
+import net.sigmabeta.chipbox.player.speaker.Speaker
+
+class RealSpeaker() : Speaker {
+    override fun play(bytes: ByteArray) {
+        TODO("Not yet implemented")
+    }
+}
