69e44ac6687b2a340a2abd4cdd252de0c1bd877c
sigmabeta <bastos.eder@gmail.com>
2021-09-02 17:07:27 -0400

    Write unit test that writes to a file

---
+++ core/player/speaker/file/src/test/java/net/sigmabeta/chipbox/player/speaker/file/FileSpeakerTest.kt
@@ -0,0 +1,56 @@
+package net.sigmabeta.chipbox.player.speaker.file
+
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import net.sigmabeta.chipbox.models.Track
+import net.sigmabeta.chipbox.player.generator.fake.FakeGenerator
+import net.sigmabeta.chipbox.player.generator.fake.TrackRandomizer
+import net.sigmabeta.chipbox.repository.Repository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.File
+
+
+internal class FileSpeakerTest {
+
+    private val track: Track = mockk()
+
+    private val repository: Repository = mockk()
+
+    private val generator = FakeGenerator(
+        2000,
+        2048,
+        TrackRandomizer(repository)
+    )
+
+    private lateinit var underTest: FileSpeaker
+
+    @BeforeEach
+    fun setUp() {
+        every { repository.getTrack(any()) } returns track
+        every { track.trackLengthMs } returns 10_000L
+
+        underTest = FileSpeaker(
+            2000,
+            File(System.getProperty("user.home")),
+            generator,
+            Dispatchers.Default
+        )
+    }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `When a play command is issued, a valid WAV file is created`() = runBlocking {
+        underTest.play(ID_TRACK_ARBITRARY)
+
+        assert(underTest.bytesWritten != 0)
+    }
+
+    companion object {
+        const val ID_TRACK_ARBITRARY = 1234L
+    }
+}
