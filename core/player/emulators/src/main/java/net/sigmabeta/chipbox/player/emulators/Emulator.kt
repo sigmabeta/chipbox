71d7f2fc31382d2f017a4a300b29f038d3afbe4d
sigmabeta <bastos.eder@gmail.com>
2021-09-05 11:52:59 -0400

    Continue refactoring to separate "Emulator" from "Generator"

--- core/player/emulators/src/main/java/net/sigmabeta/chipbox/player/emulators/Emulator.kt
+++ core/player/emulators/src/main/java/net/sigmabeta/chipbox/player/emulators/Emulator.kt
@@ -1,15 +1,57 @@
 package net.sigmabeta.chipbox.player.emulators
 
 import net.sigmabeta.chipbox.models.Track
+import net.sigmabeta.chipbox.player.common.SHORTS_PER_FRAME
+import net.sigmabeta.chipbox.player.common.millisToFrames
 
-interface Emulator {
-    var sampleRate: Int
+abstract class Emulator {
+    var trackOver: Boolean = false
 
-    var trackOver: Boolean
+    abstract var sampleRate: Int
 
-    fun loadTrack(track: Track)
+    abstract fun loadTrackInternal(track: Track)
 
-    fun generateBuffer(buffer: ShortArray): Int
+    abstract fun generateBufferInternal(buffer: ShortArray, framesPerBuffer: Int): Int
 
-    fun teardown()
+    abstract fun teardownInternal()
+
+    private var framesPlayedTotal = 0
+
+    private var remainingFramesTotal = -1
+
+    fun loadTrack(track: Track) {
+        if (remainingFramesTotal >= 0) {
+            println("Previously loaded emulator not cleared. Clearing...")
+            teardown()
+        }
+
+        remainingFramesTotal = track.trackLengthMs.toDouble().millisToFrames(sampleRate)
+
+        loadTrackInternal(track)
+    }
+
+    fun generateBuffer(
+        buffer: ShortArray
+    ): Int {
+        if (remainingFramesTotal < 0) {
+            trackOver = true
+            return -1
+        }
+
+        val framesPerBuffer = buffer.size / SHORTS_PER_FRAME
+
+        val framesPlayed = generateBufferInternal(buffer, framesPerBuffer)
+
+        framesPlayedTotal += framesPlayed
+        remainingFramesTotal -= framesPlayed
+
+        return framesPlayed
+    }
+
+    fun teardown() {
+        teardownInternal()
+
+        remainingFramesTotal = -1
+        framesPlayedTotal = 0
+    }
 }
