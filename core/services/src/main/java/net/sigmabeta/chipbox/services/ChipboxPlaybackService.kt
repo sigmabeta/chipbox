b3e3974f9a3bba6f03f70bc7edf95ab098f9bf72
sigmabeta <bastos.eder@gmail.com>
2021-10-07 20:41:32 -0400

    Implement accessing repository's top level lists

--- core/services/src/main/java/net/sigmabeta/chipbox/services/ChipboxPlaybackService.kt
+++ core/services/src/main/java/net/sigmabeta/chipbox/services/ChipboxPlaybackService.kt
@@ -2,16 +2,32 @@ package net.sigmabeta.chipbox.services
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_BROWSABLE
+import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.media.MediaBrowserServiceCompat
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import net.sigmabeta.chipbox.repository.Data
+import net.sigmabeta.chipbox.repository.Repository
+import net.sigmabeta.chipbox.services.transformers.toMediaItem
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ChipboxPlaybackService : MediaBrowserServiceCompat() {
     @Inject
     lateinit var callback: ChipboxServiceCallback
+
+    @Inject
+    lateinit var repository: Repository
+
+    private val serviceScope = CoroutineScope(Dispatchers.Default)
 
     private var mediaSession: MediaSessionCompat? = null
     private lateinit var stateBuilder: PlaybackStateCompat.Builder
@@ -61,30 +77,73 @@ class ChipboxPlaybackService : MediaBrowserServiceCompat() {
             return
         }
 
-        val mediaItems = when (parentMediaId) {
-            ID_ROOT_FULL -> getTopLevelMenuItems()
-            ID_ROOT_GAMES -> getGamesMenuItems()
-            else -> {
-                result.sendError(Bundle.EMPTY)
-                return
-            }
+        if (parentMediaId == ID_ROOT_FULL) {
+            result.sendResult(getTopLevelMenuItems())
+            return
         }
 
-        result.sendResult(mediaItems)
+        result.detach()
+
+        serviceScope.launch {
+            val mediaItems = when (parentMediaId) {
+                ID_GAMES -> getGamesMenuItems()
+                ID_ARTISTS -> getArtistsMenuItems()
+                else -> {
+                    result.sendError(Bundle.EMPTY)
+                    return@launch
+                }
+            }
+
+            result.sendResult(mediaItems)
+        }
     }
 
     private fun getTopLevelMenuItems(): List<MediaBrowserCompat.MediaItem> {
-        TODO("Not yet implemented")
+        return listOf(
+            topLevelItemGames(),
+            topLevelItemArtists()
+        )
     }
 
-    private fun getGamesMenuItems(): List<MediaBrowserCompat.MediaItem>? {
-        TODO("Not yet implemented")
-    }
+    private fun topLevelItemGames() = MediaBrowserCompat.MediaItem(
+        MediaDescriptionCompat.Builder()
+            .setMediaId(ID_GAMES)
+            .setTitle("Games")
+            .setDescription("Your Chipbox library, sorted by game title.")
+            .build(),
+        FLAG_BROWSABLE
+    )
+
+    private fun topLevelItemArtists() = MediaBrowserCompat.MediaItem(
+        MediaDescriptionCompat.Builder()
+            .setMediaId(ID_ARTISTS)
+            .setTitle("Artists")
+            .setDescription("Your Chipbox library, sorted by artist name.")
+            .build(),
+        FLAG_BROWSABLE
+    )
+
+    private suspend fun getGamesMenuItems() = repository
+            .getAllGames(false, false)
+            .filter { it is Data.Succeeded }
+            .map { it as Data.Succeeded }
+            .map { it.data }
+            .first()
+            .map { it.toMediaItem() }
+
+
+    private suspend fun getArtistsMenuItems() = repository
+        .getAllArtists(false, false)
+        .filter { it is Data.Succeeded }
+        .map { it as Data.Succeeded }
+        .map { it.data }
+        .first()
+        .map { it.toMediaItem() }
+
 
     private fun allowBrowsing(clientPackageName: String, clientUid: Int): AccessLevel {
         return AccessLevel.FULL
     }
-
 
     companion object {
         private const val ID_ROOT_INFIX = ".media.id.root."
@@ -93,7 +152,8 @@ class ChipboxPlaybackService : MediaBrowserServiceCompat() {
         private const val ID_ROOT_FULL = ID_ROOT + "full"
         private const val ID_ROOT_EMPTY = ID_ROOT + "empty"
 
-        private const val ID_ROOT_GAMES = ID_ROOT + "games"
+        const val ID_GAMES = ID_ROOT + "games"
+        const val ID_ARTISTS = ID_ROOT + "artists"
 
         private const val LOG_TAG = "ChipboxPlaybackService"
     }
